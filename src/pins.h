6ec30b4eb61b1f189c823e5a52b8cb0679839ddc
EhWhoAmI <eh@ehwhoami.com>
2025-08-10 21:06:24 -0700

    Initial commit and initial SPI driver

---
+++ src/pins.h
@@ -0,0 +1,17 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define MOSI 12
+#define SCK 13
+#define CS 14
+#define LED_BLUE 15
+#define LED_GREEN 16
+#define LED_ORANGE 17
+#define LED_RED 18
+#define I0 3
+#define I1 4
+#define Q0 5
+#define Q1 6
+#define CLKOUT 7
+
+#endif
